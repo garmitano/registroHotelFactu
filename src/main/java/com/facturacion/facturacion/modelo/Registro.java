0af1cc80251c355236ad19cc0b7e7915315e8354
garmitano <garmitano@gmail.com>
2023-05-15 09:20:40 -0300

    feat: complete basic entities and views

--- src/main/java/com/facturacion/facturacion/modelo/Registro.java
+++ src/main/java/com/facturacion/facturacion/modelo/Registro.java
@@ -6,21 +6,29 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import org.openxava.annotations.DefaultValueCalculator;
+import org.openxava.annotations.DescriptionsList;
 import org.openxava.annotations.Hidden;
 import org.openxava.annotations.ListProperties;
+import org.openxava.annotations.NoFrame;
+import org.openxava.annotations.ReferenceView;
 import org.openxava.annotations.Required;
+import org.openxava.annotations.Stereotype;
+import org.openxava.annotations.View;
 import org.openxava.calculators.CurrentLocalDateCalculator;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity @Getter @Setter
+@View(members = "date, room;" + "detalles")
 public class Registro {
 
     @Id
@@ -30,9 +38,12 @@ public class Registro {
 
     @Required
     @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    @Stereotype("FECHA")
     private LocalDate date;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @NoFrame
+    @ReferenceView("simple")
     private Room room;
 
     @Column
--- src/main/java/com/facturacion/facturacion/modelo/Room.java
+++ src/main/java/com/facturacion/facturacion/modelo/Room.java
@@ -8,19 +8,21 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import org.openxava.annotations.Required;
+import org.openxava.annotations.View;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity @Getter @Setter
+@View(name = "simple", members = "id, tipo")
 public class Room {
     @Id
+    @Required
     private int id;
     @Column(length = 50)
     @Required(message = "Tipo de habitacion")
     private String tipo;
     @Column(length = 50)    
-    @Required
     private String descripcion;
 
     @OneToMany(mappedBy = "room")
